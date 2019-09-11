376b5f0a5b410ef8974d9643bcd96bb8dbaa002e
Mandeep30 <mandeep.rajpal3003@gmail.com>
2019-09-12 01:45:42 +0530

    Added original ArraySubscription implementation

---
+++ src/main/java/org/neonex/publisher/ImperativeArraySubscription.java
@@ -0,0 +1,52 @@
+package org.neonex.publisher;
+
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
+
+@Deprecated(forRemoval = true)
+final class ImperativeArraySubscription<T> implements Subscription {
+    private Subscriber<? super T> subscriber;
+    private T[] array;
+    private int startIndex = 0;
+    private boolean isCompleted = false;
+    private boolean workInProgress = false;
+    private int requestedElements = 0;
+
+    public ImperativeArraySubscription(T[] array, Subscriber<? super T> subscriber) {
+        this.array = array;
+        this.subscriber = subscriber;
+    }
+
+    @Override
+    public void request(long numberOfElements) {
+        if (!isCompleted) {
+            requestedElements += (int) numberOfElements;
+            if (requestedElements > array.length) {
+                requestedElements = array.length - startIndex;
+            }
+            if (workInProgress) {
+                return;
+            }
+            for (int i = startIndex; i < startIndex + requestedElements; i++) {
+                workInProgress = true;
+                if (array[i] == null) {
+                    subscriber.onError(new NullPointerException());
+                    return;
+                }
+                subscriber.onNext(array[i]);
+            }
+            startIndex = startIndex + requestedElements;
+            requestedElements = 0;
+            workInProgress = false;
+            if (startIndex == array.length) {
+                subscriber.onComplete();
+                isCompleted = true;
+            }
+        }
+    }
+
+    @Override
+    public void cancel() {
+
+    }
+}
